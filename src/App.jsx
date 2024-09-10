b0b5849ca70c75149b9e254e33bce6a0f07219b0
Vaibhav262610 <branda56235@gmail.com>
2024-09-10 21:44:04 +0530

    h

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,20 @@ import React from "react";
 import Header from "./components/Header";
 import Mid from "./components/Mid";
 import Footer from "./components/Footer";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { Donut } from "./components/Donut";
 
 const App = () => {
   return (
     <>
+      <div className="h-screen absolute top-0 w-full">
+        <Canvas shadows>
+          <ambientLight intensity={2} />
+          {/* <OrbitControls /> */}
+          <Donut />
+        </Canvas>
+      </div>
       <Header />
       <Mid />
       <Footer />
---
+++ src/components/Donut.jsx
@@ -0,0 +1,44 @@
+import React, { useRef } from "react";
+import { useGLTF } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+
+export function Donut(props) {
+  const { nodes, materials } = useGLTF("/donut.glb");
+  const donutRef = useRef();
+
+  useFrame(() => {
+    if (donutRef.current) {
+      donutRef.current.rotation.y += 0.001; // Rotate around the Y axis
+    }
+  });
+  return (
+    <group
+      ref={donutRef}
+      {...props}
+      dispose={null}
+      scale={(12, 12, 12)}
+      position={(0, 0, 0)}
+      rotation={[Math.PI / 4, 0, 0]}
+    >
+      <mesh
+        geometry={nodes.Object_4.geometry}
+        material={materials.donut}
+        position={[0, 0.037, 0]}
+      />
+      <mesh
+        geometry={nodes.Object_6.geometry}
+        material={materials.icing}
+        position={[0, 0.037, 0]}
+      />
+      <mesh
+        geometry={nodes.Object_8.geometry}
+        material={materials.sprinkles_bake}
+        position={[0.076, 0.082, 0.009]}
+        rotation={[1.448, 0.073, 3.007]}
+        scale={0.141}
+      />
+    </group>
+  );
+}
+
+useGLTF.preload("/donut.glb");
